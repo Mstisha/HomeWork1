837f16f0c4b304b4da6bba473d1052fe39e87bb1
Slava <mistik99@mail.ru>
2023-01-16 12:08:21 +0200

    Задача семинара №2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:");
+string numbersStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numbersStr1);
+
+Console.WriteLine("Введите второе число:");
+string numbersStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numbersStr2);
+int max = 0;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.WriteLine($"Максимальное и наибольшее число {max}");
