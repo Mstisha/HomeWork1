acbb4a91b4bf6925b4b2eb3c96753ea4da24e089
Slava <mistik99@mail.ru>
2023-01-17 08:41:32 +0200

    Задача семинара №4

---
+++ Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите Ваше число:");
+string numbersStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numbersStr1);
+
+Console.WriteLine("Введите Ваше второе число:");
+string numbersStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numbersStr2);
+
+Console.WriteLine("Введите Ваше третье число:");
+string numbersStr3 = Console.ReadLine();
+int c = Convert.ToInt32(numbersStr3);
+
+if (a > b & a > c)
+{
+    Console.WriteLine(a);
+}
+else if (b > a & b > c)
+{
+    Console.WriteLine(b);
+}
+else if (c > a & c > b)
+{
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("Числа равны!");
+}
