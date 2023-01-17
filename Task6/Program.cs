e42621f1674774ab70b61a0f9a896a1414b15941
Slava <mistik99@mail.ru>
2023-01-17 08:47:10 +0200

    Задача семинара №6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("ДА!");
+}
+else
+{
+    Console.WriteLine("НЕТ!");
+}
