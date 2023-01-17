c73587629308dc2aa14ddc11c00f2068c0179218
Slava <mistik99@mail.ru>
2023-01-17 08:56:24 +0200

    Задача семинара №8

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+for (int i = 1; i < number; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + ",");
+    }
+}
